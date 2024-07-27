d70c59b6478f93ed98a28d4affae57d92f419139
Benjamin McLemore <mclemorebenjamin@gmail.com>
2024-07-27 13:24:21 -0400

    Change id to longer-days

--- src/main/java/brmv/longer_days/LongerDays.java
+++ src/main/java/brmv/longer_days/LongerDays.java
@@ -9,7 +9,7 @@ public class LongerDays implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("longer_days");
+    public static final Logger LOGGER = LoggerFactory.getLogger("longer-days");
 
 	@Override
 	public void onInitialize() {
