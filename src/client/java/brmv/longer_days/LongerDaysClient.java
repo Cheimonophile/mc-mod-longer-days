38df2ce12167e20f5242ab23953be14bc7089162
Benjamin McLemore <mclemorebenjamin@gmail.com>
2024-07-27 13:15:37 -0400

    initial commit

---
+++ src/client/java/brmv/longer_days/LongerDaysClient.java
@@ -0,0 +1,10 @@
+package brmv.longer_days;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LongerDaysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
